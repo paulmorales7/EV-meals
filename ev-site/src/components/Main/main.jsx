7c25f55dd5dbf80fea067a005141e19b9a7c0e91
Morales <pmorales@activision.com>
2021-11-27 15:30:33 -0600

    created template for main page with both sections

--- ev-site/src/components/Main/main.jsx
+++ ev-site/src/components/Main/main.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import '../Main/main.css'
 
 const Main = () => {
-    return(
+    return (
 
-        <div>hi</div>
+        <div className='section'>
+            <div className='info'>
+                <h2>paragraph</h2>
+            </div>
 
-    )}
+            <div className='pic'>
+                <h3>image</h3>
+            </div>
+
+        </div>
+
+
+    )
+}
 
 export default Main;
