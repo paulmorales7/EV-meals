ab158235ce9e33dd60feb2fd341b03386751ae5c
Morales <pmorales@activision.com>
2021-12-05 17:37:49 -0600

    added text and pic to main page, working on placing and sizing of info

--- ev-site/src/components/Main/main.jsx
+++ ev-site/src/components/Main/main.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import '../Main/main.css'
+import '../Main/main.css';
+import veganFood from '../../images/vegan_flick-removebg.png';
 
 const Main = () => {
     return (
 
         <div className='section'>
             <div className='info'>
-                <h2>paragraph</h2>
+                <h2>Lorem ipsum, dolor sit amet consectetur adipisicing <br /> elit. Tempora, recusandae?</h2>
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
             </div>
 
-            <div className='pic'>
-                <h3>image</h3>
+            <div className='pic'>                   
+              <img className='image' src={veganFood}/>
             </div>
 
         </div>
